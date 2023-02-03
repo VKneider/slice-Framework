4f58995f703098b164b50fd394328d7f8f9e80eb
Victor Kneider <victorkneider@gmail.com>
2023-02-03 02:01:26 -0400

    feat: added load function

--- Slice/Slice.js
+++ Slice/Slice.js
@@ -1,21 +1,31 @@
-import Controller from "./controller/Controller.js";
+//import x → X ACCEDE a esa variable de slice // window.slice aun no existe
 
-export default class Slice {
-    constructor () {
-        this.styles = document.createElement('style');
-        document.head.appendChild(this.styles);
-        this.controller= new Controller();
-
-        let templatesContainer = document.createElement("div");
-        templatesContainer.id = "templates-container";
-        document.head.appendChild(templatesContainer);
-    }
-
-    connectedCallback() {
-    }
-
-
+ export default class Slice {
+     constructor () {
+         this.styles = document.createElement('style');
+         this.styles.id="styles-slice"
+         document.head.appendChild(this.styles);
+         this.controller;
+        }
+        connectedCallback() {
+            
+        } 
 }
 
 customElements.define("my-slice", Slice);
-window.slice= new Slice();
+window.slice= new Slice(); 
+
+async function load(){
+    //se puede mejorar utilizando reflection
+    let x = await import("./controller/Controller.js");
+    window.slice.controller = new x.default();
+    await import ("./components/front/img-carousel/img-carousel.js");
+    await import ("./components/front/SliderPro/SliderPro.js")
+    await import("./components/front/Navbar/Navbar.js")
+}
+
+load();
+
+
+
+
